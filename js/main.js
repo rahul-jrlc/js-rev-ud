bb7ecb1bcc4fa6bc52c39bd19c436b8a934b2d90
Rahul <rahul.pandita41299@gmail.com>
2022-08-24 13:57:16 +0530

    coding challenge 1

--- js/main.js
+++ js/main.js
@@ -39,7 +39,7 @@ let isIsland = false;
 let language;
 console.log(typeof isIsland);
 console.log(typeof language);
-*/
+
 
 // LET, CONST , VAR
 let age = 30;
@@ -55,3 +55,38 @@ job = "teacher";
 
 lastName = "pinky"; // CREATES A PROPERTY ON THE GLOBAL OBJECT, NOT A VARIABLE IN CURRENT SCOPE
 console.log(lastName);
+*/
+
+// CODING CHALLENGE 1
+/*
+Mark and John are trying to compare their BMI (Body Mass Index), which is
+calculated using the formula:
+BMI = mass / height ** 2 = mass / (height * height) (mass in kg
+and height in meter).
+Your tasks:
+1. Store Mark's and John's mass and height in variables
+2. Calculate both their BMIs using the formula (you can even implement both
+versions)
+3. Create a Boolean variable 'markHigherBMI' containing information about
+whether Mark has a higher BMI than John.
+Test data:
+§ Data 1: Marks weights 78 kg and is 1.69 m tall. John weights 92 kg and is 1.95
+m tall.
+§ Data 2: Marks weights 95 kg and is 1.88 m tall. John weights 85 kg and is 1.76
+m tall.
+
+
+const markMass = 78;
+const markHeight = 1.69;
+const johnMass = 92;
+const johnHeight = 1.95;
+
+const markBMI = markMass / markHeight ** 2;
+console.log(markBMI);
+
+const johnBMI = johnMass / johnHeight ** 2;
+console.log(johnBMI);
+
+const markHigherBMI = markBMI > johnBMI;
+console.log(markHigherBMI);
+*/
