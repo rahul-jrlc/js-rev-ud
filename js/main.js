cea0eacc650a022d71ec610165852777e56e5cab
Rahul <rahul.pandita41299@gmail.com>
2022-08-24 10:52:40 +0530

    values and variables

---
+++ js/main.js
@@ -0,0 +1,16 @@
+let js = "amazing";
+console.log(20 + 30 + 60);
+
+console.log("Jonas");
+console.log(7474);
+
+let firstName = "Jonas";
+console.log(firstName);
+
+let country = "India";
+let continent = "Asia";
+let population = 1300000000;
+
+console.log(country);
+console.log(continent);
+console.log(population);
