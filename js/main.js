bc284eb1d9cf74b7a6a68f7abf5609d79c695d42
Rahul <rahul.pandita41299@gmail.com>
2022-08-24 11:04:51 +0530

    data types

--- js/main.js
+++ js/main.js
@@ -1,3 +1,4 @@
+/*
 let js = "amazing";
 console.log(20 + 30 + 60);
 
@@ -14,3 +15,27 @@ let population = 1300000000;
 console.log(country);
 console.log(continent);
 console.log(population);
+*/
+
+// DATA TYPES - Primitives and objects
+let isFun = true;
+console.log(isFun);
+
+console.log(typeof true);
+console.log(typeof "Hello");
+
+isFun = "NOPE";
+
+let year;
+console.log(year);
+console.log(typeof year);
+
+year = 1999;
+console.log(typeof year);
+
+console.log(typeof null);
+
+let isIsland = false;
+let language;
+console.log(typeof isIsland);
+console.log(typeof language);
