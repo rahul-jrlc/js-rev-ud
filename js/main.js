be1c1927d9ab1f121ca9ce3a7cb8faa31869e970
Rahul <rahul.pandita41299@gmail.com>
2022-08-25 17:36:39 +0530

    coding challenge 3

--- js/main.js
+++ js/main.js
@@ -202,7 +202,7 @@ console.log(18 == "18"); // loose equality operator - type coercion
 
 const fav = Number(prompt("Favourite number?"));
 console.log(fav);
-*/
+
 
 // BOOLEAN LOGIC
 
@@ -212,3 +212,42 @@ const hasGoodVision = false;
 console.log(hasDriversLicense && hasGoodVision);
 console.log(hasDriversLicense || hasGoodVision);
 console.log(!hasDriversLicense);
+*/
+
+// CODING CHALLENGE 3
+/*
+There are two gymnastics teams, Dolphins and Koalas. They compete against each
+other 3 times. The winner with the highest average score wins a trophy!
+Your tasks:
+1. Calculate the average score for each team, using the test data below
+2. Compare the team's average scores to determine the winner of the competition,
+and print it to the console. Don't forget that there can be a draw, so test for that
+as well (draw means they have the same average score)
+3. Bonus 1: Include a requirement for a minimum score of 100. With this rule, a
+team only wins if it has a higher score than the other team, and the same time a
+score of at least 100 points. Hint: Use a logical operator to test for minimum
+score, as well as multiple else-if blocks �
+4. Bonus 2: Minimum score also applies to a draw! So a draw only happens when
+both teams have the same score and both have a score greater or equal 100
+points. Otherwise, no team wins the trophy
+Test data:
+§ Data 1: Dolphins score 96, 108 and 89. Koalas score 88, 91 and 110
+*/
+
+const dolphinsScore = (50 + 50 + 1) / 2;
+console.log(dolphinsScore);
+
+const KoalasScore = (100 + 100 + 0) / 2;
+console.log(KoalasScore);
+
+if (dolphinsScore >= 100 || KoalasScore >= 100) {
+    if (dolphinsScore > KoalasScore) {
+        console.log(`Winner: Dolphins with a score of ${dolphinsScore}.`);
+    } else if (KoalasScore > dolphinsScore) {
+        console.log(`Winner: Koalas with a score of ${KoalasScore}.`);
+    } else {
+        console.log("IT'S A DRAW.");
+    }
+} else {
+    console.log("Scores too low.");
+};
