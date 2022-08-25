d4b23ee5084f12fcc6ea4989508026ac85b10347
Rahul <rahul.pandita41299@gmail.com>
2022-08-25 20:07:00 +0530

    function declarations vs expressions

--- js/main.js
+++ js/main.js
@@ -1,7 +1,7 @@
 "use strict";
 
 // FUNCTIONS
-
+/*
 function logger() {
     console.log("My name is Pinky.");
 };
@@ -22,3 +22,55 @@ const orangeJuice = fruitProcessor(0, 3);
 console.log(appleJuice);
 console.log(orangeJuice);
 console.log(juice1);
+*/
+
+// FUNCTION DECLARATION VS EXPESSION
+
+// FUNCTION DECLARATION
+function calcAge1(birthYear) {
+    // const age = 2037 - birthYear;
+    return 2037 - birthYear;
+};
+
+const age1 = calcAge1(1999);
+console.log(age1);
+
+
+// FUNCTION EXPRESSION
+const calcAge2 = function (birthYear) {
+    return 2037 - birthYear;
+};
+const age2 = calcAge2(1991);
+console.log(age2);
+
+function describeCountry(country, population, capitalCity) {
+    return `${country} has ${population} people and its capital city is ${capitalCity}.`
+};
+const india = describeCountry("India", "1.3 billion", "New Delhi");
+console.log(india);
+
+const finland = describeCountry("Finland", "6 million", "Helsinki");
+console.log(finland);
+
+const bangladesh = describeCountry("Azerbaijan", "2 million", "Dhaka");
+console.log(bangladesh);
+
+function percentageOfWorld1(population) {
+    return (population / 7900) * 100;
+};
+
+const argentina = percentageOfWorld1(3);
+console.log(argentina);
+
+const usa = percentageOfWorld1(30);
+console.log(usa);
+
+const vatican = percentageOfWorld1(2);
+console.log(vatican);
+
+const percentageOfWorld2 = function (population) {
+    return (population / 7900) * 100;
+};
+
+const india2 = percentageOfWorld2(130);
+console.log(india2);
