3b32a88ba14ba9fcac7fff79b6fc892da4bbea0f
Rahul <rahul.pandita41299@gmail.com>
2022-08-27 14:06:23 +0530

    Guess my number - selecting and manipulating elements

--- js/main.js
+++ js/main.js
@@ -1,5 +1,17 @@
 "use strict";
 
-// JAVASCRIPT IN THE BROWSER: DOM
+// JAVASCRIPT IN THE BROWSER: DOM MANIPULATION
 
 // PROJECT - GUESS MY NUMBER
+
+console.log(document.querySelector(".message").textContent);
+
+document.querySelector(".message").textContent = "Correct Number!";
+
+console.log(document.querySelector(".message").textContent);
+
+document.querySelector(".number").textContent = 13;
+document.querySelector(".score").textContent = 1000;
+
+document.querySelector(".guess").value = 23;
+console.log(document.querySelector(".guess").value);
