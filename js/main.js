b474b135e0259a00b5fcef98d28987200c7f4e18
Rahul <rahul.pandita41299@gmail.com>
2022-08-27 14:33:49 +0530

    Guess my number - implementing game logic

--- js/main.js
+++ js/main.js
@@ -17,14 +17,42 @@ document.querySelector(".guess").value = 23;
 console.log(document.querySelector(".guess").value);
 */
 
+// IMPLEMENTING GAME LOGIC
 
-// HANDLING CLICK EVENTS
+const secretNumber = Math.trunc(Math.random() * 20) + 1;
 
+let score = 20; // state variable
+
+document.querySelector(".number").textContent = secretNumber;
+
+//   -------------------handling click events
 document.querySelector(".check").addEventListener("click", function () {
     const guess = Number(document.querySelector(".guess").value);
     console.log(guess);
 
     if (!guess) {
-        document.querySelector(".message").textContent = "No Number :("
+        document.querySelector(".message").textContent = "No Number :(";
+    } else if (guess === secretNumber) {
+        document.querySelector(".message").textContent = "Correct Number :)"
+    } else if (guess > secretNumber) { 
+        if (score > 1) {
+            document.querySelector(".message").textContent = "Too high!";
+            score--;
+            document.querySelector(".score").textContent = score;
+        } else {
+            document.querySelector(".message").textContent = "LOSER :|"
+            document.querySelector(".score").textContent = 0;
+        }
+        
+    } else if (guess < secretNumber) {
+        if (score > 1) {
+            document.querySelector(".message").textContent = "Too Low!";
+            score--;
+            document.querySelector(".score").textContent = score;
+        } else {
+            document.querySelector(".message").textContent = "LOSER :|";
+            document.querySelector(".score").textContent = 0;
+        }
+        
     }
-})
+});
