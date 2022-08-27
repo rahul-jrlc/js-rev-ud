49a520856f6123476f222d56fe814a76d188aa7f
Rahul <rahul.pandita41299@gmail.com>
2022-08-27 14:16:10 +0530

    guess my number - handling click events

--- js/main.js
+++ js/main.js
@@ -3,7 +3,7 @@
 // JAVASCRIPT IN THE BROWSER: DOM MANIPULATION
 
 // PROJECT - GUESS MY NUMBER
-
+/*
 console.log(document.querySelector(".message").textContent);
 
 document.querySelector(".message").textContent = "Correct Number!";
@@ -15,3 +15,16 @@ document.querySelector(".score").textContent = 1000;
 
 document.querySelector(".guess").value = 23;
 console.log(document.querySelector(".guess").value);
+*/
+
+
+// HANDLING CLICK EVENTS
+
+document.querySelector(".check").addEventListener("click", function () {
+    const guess = Number(document.querySelector(".guess").value);
+    console.log(guess);
+
+    if (!guess) {
+        document.querySelector(".message").textContent = "No Number :("
+    }
+})
