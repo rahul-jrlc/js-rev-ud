9c1088cee329edbbd02cdbf73a58d706d632e0ef
Rahul <rahul.pandita41299@gmail.com>
2022-08-31 18:16:15 +0530

    the for-of loop

--- js/main.js
+++ js/main.js
@@ -43,6 +43,19 @@ const restaurant = {
     }
 };
 
+// THE FOR-OF LOOP
+const menu = [...restaurant.starterMenu, ...restaurant.mainMenu];
+
+for (const item of menu) console.log(item);
+
+for (const [i, el] of menu.entries()) {
+    console.log(`${i + 1}: ${el}.`);
+};
+
+console.log([...menu.entries()]);
+
+
+/*
 restaurant.orderPizza("mushroom", "onion", "capsicum", "jalapeno", "tomato");
 
 // 1) DESTRUCTURING
