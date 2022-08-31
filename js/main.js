3a15a770495b352b7a02812c6bb4ca7282537396
Rahul <rahul.pandita41299@gmail.com>
2022-08-31 16:07:17 +0530

    destructuring objects

--- js/main.js
+++ js/main.js
@@ -2,7 +2,6 @@
 
 // DATA STRUCTURES, MODERN OPERATORS AND STRINGS
 
-// DESTRUCTURING ARRAYS
 
 const restaurant = {
     name: 'Classico Italiano',
@@ -13,9 +12,82 @@ const restaurant = {
 
     order: function (starterIndex, mainIndex) {
         return [this.starterMenu[starterIndex], this.mainMenu[mainIndex]];
+    },
+    openingHours: {
+        thu: {
+            open: 12,
+            close: 22,
+        },
+        fri: {
+            open: 11,
+            close: 23,
+        },
+        sat: {
+            open: 0, // Open 24 hours
+            close: 24,
+        },
+    },
+
+    orderDelivery: function ({starterIndex = 1, mainIndex = 0, time = "20:00", address}) {
+        console.log(`Order received: ${this.starterMenu[starterIndex]} and ${this.mainMenu[mainIndex]} will be delivered to ${address} at ${time}.`);
     }
 };
 
+restaurant.orderDelivery(
+    {
+        time: "22:30",
+        address: "Tihar Jail",
+        mainIndex: 2,
+        starterIndex: 2
+    }
+);
+
+restaurant.orderDelivery(
+    {
+        address: "Tihar Jail",
+        starterIndex: 1
+    }
+);
+
+// DESTRUCTURING OBJECTS
+const { name, categories, openingHours } = restaurant;
+console.log(name);
+console.log(categories);
+console.log(openingHours);
+
+const { name: restaurantName, openingHours: hours, categories: tags } = restaurant;
+console.log(restaurantName);
+console.log(hours);
+console.log(tags);
+
+// Default values
+const { menu = [], starterMenu: starters = [], mainMenu: mainCourse = [] } = restaurant;
+console.log(menu);
+console.log(starters);
+console.log(mainCourse);
+
+// Mutating variables
+let a = 3232;
+let b = 4343;
+let c = 41299;
+let d = 0;
+const obj = { a: 23, b: 7, c: 14 };
+
+({ a, b, c, d=100000 } = obj);
+console.log(a);
+console.log(b);
+console.log(c);
+console.log(d);
+
+// Nested objects
+const { fri: {open: openingTime, close: closingTime} } = openingHours
+console.log(openingTime);
+console.log(closingTime);
+
+
+/*
+//////////////////////////////////////////////////////////////
+// DESTRUCTURING ARRAYS
 const arr = [2, 3, 4];
 const a = arr[0];
 const b = arr[1];
@@ -60,3 +132,4 @@ console.log(i, p, q);
 // Default values
 const [j = 1, k = 1, r = 1] = [8, 9];
 console.log(j, k, r);
+*/
