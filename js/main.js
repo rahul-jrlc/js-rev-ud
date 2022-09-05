8c22699b7f2412b0383bf8476d6e83fd236ed747
Rahul <rahul.pandita41299@gmail.com>
2022-09-05 14:11:14 +0530

    Functions - default parameters

--- js/main.js
+++ js/main.js
@@ -3,3 +3,27 @@
 // A CLOSER LOOK AT FUNCTIONS
 
 // Default parameters
+
+const bookings = [];
+
+const creatBooking = function (flightNum, numPassengers = 1, price = numPassengers * 178) {
+    // pre ES6 way to set default values
+    // numPassengers = numPassengers || 1;
+    // price = price || 199;
+    
+    const booking = {
+        flightNum,
+        numPassengers,
+        price
+    }
+    console.log(booking);
+    console.log(`Flight number: ${flightNum}, No. of passengers: ${numPassengers}, Booking cost: ${price}.`);
+    bookings.push(booking);
+
+};
+
+creatBooking('LH123');
+creatBooking('LH123', 2, 800);
+creatBooking("LH123", 3);
+
+creatBooking("LH123", undefined, 300);// skipping a default parameter
